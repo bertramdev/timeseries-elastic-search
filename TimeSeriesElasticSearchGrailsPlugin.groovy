b121fd755ab491c9262e8c0998e5878a5f10a0d1
jerleng <jerleng@gmail.com>
2014-03-18 08:36:05 -0700

    Update TimeSeriesElasticSearchGrailsPlugin.groovy

--- TimeSeriesElasticSearchGrailsPlugin.groovy
+++ TimeSeriesElasticSearchGrailsPlugin.groovy
@@ -16,24 +16,22 @@ class TimeSeriesElasticSearchGrailsPlugin {
 Elastic Search implementation of time series.
 '''
     // URL to the plugin's documentation
-    def documentation = "http://grails.org/plugin/time-series-elastic-search"
+    def documentation = "https://github.com/bertramdev/timeseries-elastic-search"
 
     // Extra (optional) plugin metadata
 
     // License: one of 'APACHE', 'GPL2', 'GPL3'
-//    def license = "APACHE"
+    def license = "APACHE"
 
     // Details of company behind the plugin (if there is one)
-//    def organization = [ name: "My Company", url: "http://www.my-company.com/" ]
+    def organization = [ name: "BertramLabs", url: "http://www.bertramlabs.com/" ]
 
-    // Any additional developers beyond the author specified above.
-//    def developers = [ [ name: "Joe Bloggs", email: "joe@bloggs.net" ]]
 
     // Location of the plugin's issue tracker.
-//    def issueManagement = [ system: "JIRA", url: "http://jira.grails.org/browse/GPMYPLUGIN" ]
+    def issueManagement = [ system: "GIT", url: "https://github.com/bertramdev/timeseries-elastic-search" ]
 
     // Online location of the plugin's browseable source code.
-//    def scm = [ url: "http://svn.codehaus.org/grails-plugins/" ]
+    def scm = [ url: "https://github.com/bertramdev/timeseries-elastic-search" ]
 
     def doWithWebDescriptor = { xml ->
         // TODO Implement additions to web.xml (optional), this event occurs before
